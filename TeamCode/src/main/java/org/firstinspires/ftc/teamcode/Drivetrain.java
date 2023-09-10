a16b4165f900637f4a75b312cd08987897d012c4
Vishwa Kalal <vishwa_kalal@Vishwas-Air.attlocal.net>
2023-09-10 15:19:07 -0400

    drivetrain code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
@@ -1,2 +1,152 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 public class Drivetrain {
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+    IMU imu;
+    YawPitchRollAngles ypr;
+    LinearOpMode opMode;
+
+    public Drivetrain(LinearOpMode opMode, HardwareMap hardwareMap){
+        frontLeft = hardwareMap.get(DcMotor.class,"frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class,"frontRight");
+        backLeft = hardwareMap.get(DcMotor.class,"backLeft");
+        backRight = hardwareMap.get(DcMotor.class,"backRight");
+
+
+        this.opMode = opMode;
+
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.FORWARD);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.FORWARD);
+        encoders();
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
+
+    }
+
+    public void encoders(){
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+    public void drive(int forward, double power){
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontLeft.setTargetPosition(forward);
+        frontRight.setTargetPosition(forward);
+        backLeft.setTargetPosition(forward);
+        backRight.setTargetPosition(forward);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        frontLeft.setPower(power);
+        frontRight.setPower(power);
+        backLeft.setPower(power);
+        backRight.setPower(power);
+
+        while(frontLeft.isBusy()){
+            opMode.telemetry.addData("frontLeft current", frontLeft.getCurrentPosition());
+            opMode.telemetry.addData("frontLeft target", frontLeft.getTargetPosition());
+
+            opMode.telemetry.addData("frontRight current", frontRight.getCurrentPosition());
+            opMode.telemetry.addData("frontRight target", frontRight.getTargetPosition());
+
+            opMode.telemetry.addData("backLeft current", backLeft.getCurrentPosition());
+            opMode.telemetry.addData("backLeft target", backLeft.getTargetPosition());
+
+            opMode.telemetry.addData("backRight current", backRight.getCurrentPosition());
+            opMode.telemetry.addData("backRight target", backRight.getTargetPosition());
+            opMode.telemetry.update();
+        }
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
+
+    }
+
+    public void strafe(int slide, double power, String direction){
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        if(direction.equals("left")){
+            frontLeft.setTargetPosition(-slide);
+            frontRight.setTargetPosition(slide);
+            backLeft.setTargetPosition(slide);
+            backRight.setTargetPosition(-slide);
+
+        } else if(direction.equals("right")){
+            frontLeft.setTargetPosition(slide);
+            frontRight.setTargetPosition(-slide);
+            backLeft.setTargetPosition(-slide);
+            backRight.setTargetPosition(slide);
+        }
+
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        frontLeft.setPower(power);
+        frontRight.setPower(power);
+        backLeft.setPower(power);
+        backRight.setPower(power);
+
+        while(frontLeft.isBusy()){
+            opMode.telemetry.addData("frontLeft current", frontLeft.getCurrentPosition());
+            opMode.telemetry.addData("frontLeft target", frontLeft.getTargetPosition());
+
+            opMode.telemetry.addData("frontRight current", frontRight.getCurrentPosition());
+            opMode.telemetry.addData("frontRight target", frontRight.getTargetPosition());
+
+            opMode.telemetry.addData("backLeft current", backLeft.getCurrentPosition());
+            opMode.telemetry.addData("backLeft target", backLeft.getTargetPosition());
+
+            opMode.telemetry.addData("backRight current", backRight.getCurrentPosition());
+            opMode.telemetry.addData("backRight target", backRight.getTargetPosition());
+            opMode.telemetry.update();
+        }
+
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
+
+    }
+
 }
